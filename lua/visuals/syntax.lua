a703f580c06f3066a00401fac6bd36b396f9af8e
Rokas Krivaitis <47082246+solidassassin@users.noreply.github.com>
2021-07-10 01:55:04 +0300

    Improve yaml syntax highlighting

--- lua/visuals/syntax.lua
+++ lua/visuals/syntax.lua
@@ -17,6 +17,14 @@ require "nvim-autopairs".setup {
 	check_ts = true;
 }
 
+vim.cmd[[
+	augroup theming
+		au!
+		au BufEnter *.{yaml,yml} :hi TSField guifg=#f56278
+		au BufEnter *{.yaml,.yml}\@<! :hi TSField guifg=g:material_variable_color
+	augroup END
+]]
+
 g.indent_blankline_char = '│'
 g.indent_blankline_use_treesitter = true
 g.indent_blankline_show_current_context = true
